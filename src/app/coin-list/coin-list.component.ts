2ca87bb1013b994d4e15ea0cb381fc41e0ac8858
Sanjula-Dulshan <sdulshan10@gmail.com>
2022-07-16 13:39:23 +0530

    get trending coins to the banner

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
-  styleUrls: ['./coin-list.component.scss']
+  styleUrls: ['./coin-list.component.scss'],
 })
 export class CoinListComponent implements OnInit {
-
-  constructor() { }
+  bannerData: any;
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    this.getBannerData();
+    this.getAllData();
   }
 
+  //get Trending Currency
+  getBannerData() {
+    return this.api.getTrendingCurrency('lkr').subscribe((res) => {
+      console.log(res);
+      this.bannerData = res;
+    });
+  }
+
+  //get all currency
+  getAllData() {
+    return this.api.getCurrency('lkr').subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
